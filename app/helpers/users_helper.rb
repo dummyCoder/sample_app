8220ddec1ff652b45a2964728b220d905b011c4a
Mohsen_Safari <mohsen.6625@yahoo.com>
2012-07-07 03:55:13 +0430

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,12 @@
 module UsersHelper
+
+	def gravatar_for(user)
+		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+		gravatar_url = "#{gravatar_id}"
+		if(File.exists?(gravatar_url))
+			image_tag(gravatar_url, :alt => user.name, :class => "gravatar")
+		else
+			image_tag("no_profile_pic.jpg", :alt => user.name, :class => "gravatar")
+		end
+	end
 end
---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define  do 
+	factory :user do
+		name "Mohsen"
+		email "mohsen.pers@gmail.com"
+		password "foobar"
+		password_confirmation "foobar"
+	end
+end
+	
